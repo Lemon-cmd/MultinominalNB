acbf10dd76653c7c712d1ac000a11ed49f4de702
Lemon-cmd <baopham4998@gmail.com>
2020-01-19 21:26:42 -0500

    Updated Code, Dataset and Fixed Errors

--- NaiveBayes.cpp
+++ NaiveBayes.cpp
@@ -1,23 +1,25 @@
 #include <string>
 #include <iostream>
-#include <fstream>
+#include <sstream>
 #include <vector>
 #include <math.h>
 #include <map>
-#include <boost/algorithm/string.hpp>
 
 using namespace std;
 
 class NaiveB
 {
     private:
+        //metadata for each state
         struct metadata
         {
             int column = NULL;
             int overall_freq = 0;
             double overall_prob = 0;
+            double stateP = NULL;
         };   
 
+        //likelihood of the edge X(i) -> Y(i)
         struct likelihood
         {
             int y_freq = 0;
@@ -25,23 +27,24 @@ class NaiveB
             double likelihood = 0;
         };
 
-        vector <vector<string> > data;
+        vector <vector<string> > data; // vector that holds the dataset
 
-        map <string, metadata*> Features;
-        map <string, metadata*> Y;
+        map <string, metadata*> Features; // X
+        map <string, metadata*> Y;  // Y or outcomes
 
-        map <string, map<string, likelihood*> > connections;
-        map <string, double> result;
+        map <string, map<string, likelihood*> > connections;    //use to store X(i) -> Y(i) connections
       
-        int data_size;
-        int num_class;
-
-        void getFeatures()
+        int data_size;  // variable that holds the size of dataset
+    
+        void getXY()
         {
+            /* Method for Putting in features and outcomes into Features and Y Maps */
+
             for (auto &row : data)
             {
                 for (int col = 0; col < row.size()-1; col ++)
                 {
+                    // if the feature is not in the map
                     if (Features.find(row[col]) == Features.end())
                     {
                         metadata* new_entryX = new metadata;
@@ -54,12 +57,13 @@ class NaiveB
                         connections[row[col]] = edge;
                         cout << "New X: " << row[col] << endl;
                     }
+                    // if the feature is already in the map, update its count
                     else
                     {
                         Features[row[col]]->overall_freq += 1;
                     }
                 }
-
+                // if the outcome is not in the map
                 if (Y.find(row[row.size()-1]) == Y.end())
                 {
                     metadata* new_entryY = new metadata;
@@ -67,6 +71,7 @@ class NaiveB
                     Y[row[row.size()-1]] = new_entryY;
                     cout << "New Y: " << row[row.size()-1] << endl;
                 }
+                // if outcome exists, update its count
                 else
                 {
                     Y[row[row.size()-1]]->overall_freq += 1;
@@ -76,6 +81,7 @@ class NaiveB
 
         void updateGProb()
         {
+            /* A method for updating X and Y global probability; Target(i)'s freq / data size */
             //updating individual X global Probability
             for (auto &feature : Features)
             {
@@ -92,7 +98,6 @@ class NaiveB
 
         void updateC()
         {
-            int pass = 0;
             //update connection between Feature and Y
             for (auto &con : connections)
             {
@@ -100,38 +105,55 @@ class NaiveB
                 {
                     for (auto &row : data)
                     {
+                        // y exists in the current row of data and the feature exists in that row
                         if (y.first == row[row.size()-1] && row[Features[con.first]->column] == con.first)
                         {
+                            // add edge to the connections map and update its freq
                             if (con.second.find(y.first) == con.second.end())
                             {
                                 likelihood* item = new likelihood();
                                 item->y_freq += 1;
                                 con.second[y.first] = item;
                             }
+                            // if already exists, simply update its freq
                             else
                             {
                                 con.second[y.first]->y_freq += 1;
                             }
-                            pass += 1;
+                        }
+                        // if feature does not match with the y value in the row
+                        else if (y.first != row[row.size()-1] && row[Features[con.first]->column] == con.first)
+                        {
+                            //simply add the edge to the connection, Map(X(current))[y(current)] = item
+                            if (con.second.find(y.first) == con.second.end())
+                            {
+                                likelihood* item = new likelihood();
+                                con.second[y.first] = item;
+                            }
                         }
                     }
-                    cout << "pass : " << pass << endl;
 
-                    // update connection probability
-                    con.second[y.first]->y_prob = double(con.second[y.first]->y_freq) / double(Features[con.first]->overall_freq);
-                    cout << con.first << " " << y.first << " prob: " << con.second[y.first]->y_prob << endl;
+                    // update edge probability
+                    if (con.second[y.first]->y_freq != 0 && Features[con.first]->overall_freq != 0)
+                    {
+                        con.second[y.first]->y_prob = double(con.second[y.first]->y_freq) / double(Features[con.first]->overall_freq);
+                    }
+                    else 
+                    {
+                        con.second[y.first]->y_prob = 0.0;
+                    }
                 }
             } 
         }
-        
-        
+
         void insert(const vector<string> &item)
-        {
+        {   
             data.push_back(item);
         }
 
         void load(string filename)
         {
+            // load file onto the vector data
             ifstream file(filename);
             string line;
 
@@ -139,109 +161,27 @@ class NaiveB
             {
                 vector<string> item;
                 istringstream input(line);
+                //split the line by space
                 for (string temp; input >> temp;)
+                    //append each string onto the current vector
                     item.push_back(temp);
+                
+                //insert the row onto the data
                 insert(item);
             }
         }   
 
-       /*
-        void updateLTable()
-        {
-            for (auto &entry : likelihood_table)
-            {
-                for (auto &y : test_feature)
-                {
-                    for (auto &row : data)
-                    {
-                        if (row[entry.first] == "1" && row[row.size()-1] == y.first)
-                        {
-                            entry.second[y.first]->yes_count += 1;
-                        }
-                    }
-                    entry.second[y.first]->yes_prob = double(entry.second[y.first]->yes_count) / double(classes[entry.first]->freq);
-                }
-            }
-        }
-
-        double totalXProb() const
-        {
-            double total_prob = 0.0;
-            for (auto &feature : classes)
-            {
-                if (total_prob == 0.0)
-                {
-                    total_prob = feature.second->prob;
-                }
-                else
-                {
-                    total_prob *= feature.second->prob;
-                }
-            }
-            
-            return total_prob;
-
-        }
-
-        void getResult()
-        {
-            double productXProb = totalXProb();
-            
-            for (auto &y : test_feature)
-            {
-                double top_equation  = 0.0;
-
-                for (auto &current : likelihood_table)
-                {
-                    if (top_equation == 0.0)
-                    {
-                        top_equation = current.second[y.first]->yes_prob;
-                    }
-                    else
-                    {
-                        top_equation *= current.second[y.first]->yes_prob;
-                    }
-
-                }
-
-                top_equation *= y.second->prob;
-                y.second->chance = double(top_equation / productXProb);                 
-            }
-
-        }
-        void setLTable()
-        {
-            
-            for (auto &entry : classes)
-            {
-                map<string, likelihood*> y_map;
-                map<string, metadata*> current;
-                for (auto &key : test_feature )
-                {
-                   
-                    likelihood* item = new likelihood();
-                    y_map[key.first] = item;
-
-                } 
-                //grab columns aka features 
-                likelihood_table[entry.first] = y_map;                   
-            }
-
-        }
-    
-    */
-
     public:     
-        NaiveB(string filename, int num_classes) 
+        NaiveB(string filename) 
         {
             load(filename);
-            num_class = num_classes;
             data_size = data.size();
-            getFeatures(); updateGProb(); updateC();
+            getXY(); updateGProb(); updateC();
         }
 
-        void displayD()
+        void displayData()
         {
+            //display the data vector
             for (auto &entry: data)
             {
                 for (auto &item : entry)
@@ -252,12 +192,13 @@ class NaiveB
             }
         }
 
-        void displayT()
+        void displayTable()
         {
+            /* A method for displaying the three maps; Features, Y, connections */
             cout << "Displaying X " << endl;
             for (auto &feature: Features)
             {
-                cout << "Feature: " << feature.first << " occurences: " << feature.second->overall_freq << " prob: " << feature.second->overall_prob << endl;
+                cout << "Feature: " << feature.first << " occurences: " << feature.second->overall_freq << " col: " << feature.second->column << " prob: " << feature.second->overall_prob << endl;
             }
 
             cout << "\nDisplaying Y " << endl;
@@ -266,7 +207,7 @@ class NaiveB
                 cout << "Y: " << y.first << " occurences: " << y.second->overall_freq << " prob: " << y.second->overall_prob << endl;
             }
 
-            cout << "\nDisplay C Table " << endl;
+            cout << "\nDisplaying Connections " << endl;
             for (auto &con : connections)
             {
                 for (auto &edge : con.second)
@@ -274,12 +215,33 @@ class NaiveB
                     cout << "Feature: " << con.first << " to Y: " << edge.first << " ocurrences: " << edge.second->y_freq << " prob: " << edge.second->y_prob << endl;
                 }
             }
+
+        }
+
+        void predict(string outlook, string temperature, string humidity, string wind)
+        {
+            // Prediction Method
+            for (auto &y : Y)
+            {
+                //Top equation P(X(i) | Y) * P(Y)
+                double top_equation = connections[outlook][y.first]->y_prob * connections[temperature][y.first]->y_prob
+                                    * connections[humidity][y.first]->y_prob * connections[wind][y.first]->y_prob 
+                                    * y.second->overall_prob;
+                
+                //P(X(i) | Y) * P(Y) / (P(X(i)))
+                y.second->stateP = double(top_equation); 
+
+                cout << "Predicting Y: " << y.first << " based on given input; probability of Y: " << y.second->stateP << endl;                
+            }
+
         }
      
 };
 
 int main()
 {
-    NaiveB classifier = NaiveB("dataset.txt", 3);
-    classifier.displayT();
+    NaiveB classifier = NaiveB("dataset.txt");
+    //classifier.displayTable();
+    classifier.predict("Sunny", "Hot", "Normal", "False");
+
 }
